3cddc2f88c6ad1b0a2bf39db3109507c2bc5af8f
Marcin Scieszka <marcin.scieszka@student.pk.edu.pl>
2021-06-11 18:56:17 +0200

    FTCS Method: saving to file results from various times

--- main.cpp
+++ main.cpp
@@ -28,12 +28,13 @@ double const lambda_dm = 0.4; // lambda value used for direct methods, NOTE: bew
 double const lambda_im = 1.0; // lambda value used for for indirect methods, NOTE: lambda = D*(dt/h*h)
 
 double **allocate_matrix(int rows, int cols);
-void print_matrix(double *const *matrix, int rows, int cols);
 void free_matrix(double *const *matrix, int rows);
-void print_array(double *array, int size);
+void print_array(double *array, int size, int print_width=6);
 void fill_array(double *array, int size, double value);
 void fill_matrix(double **matrix, int rows, int cols, double value);
 double analytical_solution(double tk, double xi);
+double second_boundary_condition(double tk, double dt);
+double first_boundary_condition();
 double find_max_error(double tk, double h, int nodes_x, int k, double *const *u);
 void ftcs_method();
 
@@ -48,62 +49,70 @@ void ftcs_method()
 {
     double dt; // delta t
     double h; // step h on the spatial grid, delta x
-    double tk; // current time on the time grid, NOTE: dt = T_MAX/(nodes_t-1)
-    double xi; // current point on the spatial grid
-    int i=0, k=0;
+    double tk; // current time on the time grid
+    double xi; // current position on the spatial grid
+    int nodes_x; // nr of nodes on the time grid, NOTE: for 201 nodes h=0.05
+    int nodes_t; // nr of nodes on the spacial grid, NOTE: for 201 nodes dt=0.01
+    double res_u; // approximate result at node x_i at time level t_k+1
+    double res_a; // analytical result
+    int i, k;
 
-    std::ofstream result_file("results_FTCS.txt");
-    if(!result_file)
+    std::cout.setf(std::ios::fixed);
+    std::ofstream FTCS_h_errors("FTCS_h-dependent_errors.txt");
+    std::ofstream FTCS_results("FTCS_results.txt");
+    if(!FTCS_h_errors || !FTCS_results)
     {
         std::cerr << "Error: file could not be opened" << std::endl;
         exit(1);
     }
 
-    int nodes_x; // nr of nodes on the time grid, NOTE: for 101 nodes h=0.1
-    int nodes_t; // nr of nodes on the spacial grid, NOTE: for 21 nodes dt=0.1
+    FTCS_h_errors << "h max_err\n";
+    FTCS_results << "h approx_res analytical_res\n";
 
-    for(nodes_x = 21; nodes_x<101; nodes_x+=5)
+//    for(nodes_x = 51; nodes_x<501; nodes_x+=50)
+    for(nodes_x = 1001; nodes_x<1002; nodes_x+=50)
     {
-        auto *ic_array = new double[nodes_x];  // initial condition array
-        fill_array(ic_array, nodes_x, 1.0); // filling values from initial condition
-
         h = a / (nodes_x - 1);
-        dt = (lambda_dm * h * h) / D; //NOTE: to calculate dt based on current spacial step h we use formula: dt=(lambda*h*h)/D
-        nodes_t = int(T_MAX / dt);
-        std::cout << std::endl << "dt: " << dt << ", " << "nr of t nodes: " << nodes_t << std::endl;
-        std::cout << "h: " << h << ", " << "nr of x nodes: " << nodes_x << std::endl;
+        nodes_t = static_cast<int>((T_MAX-T_0) / ((lambda_dm * h * h) / D)) + 1;
+        dt = T_MAX/(nodes_t-1);
 
         double **u = allocate_matrix(nodes_t, nodes_x); // matrix of approximate values
-        fill_matrix(u, nodes_t, nodes_x, 0.0);
+        fill_array(u[0], nodes_x, 1.0); // filling values from initial condition
 
-        for (i = 0; i < nodes_x; i++) u[0][i] = ic_array[i];
 
-        for (k = 0; k < nodes_t - 1; k++) {
-            for (i = 1; i < nodes_x -
-                            2; i++) // we omit first and last i - values there are already determined from the initial condition
+        tk = T_0;
+        for(k = 0; k < nodes_t - 1; k++)
+        {
+            xi = X_MIN;
+            u[k + 1][0] = first_boundary_condition();
+            xi += h;
+            for(i = 1; i < nodes_x - 1; i++) // first and last i omitted - values already determined from the initial condition
             {
-                u[k + 1][i] = lambda_dm * u[k][i - 1] + (1 - lambda_dm) * u[k][i] +
-                              lambda_dm * u[k][i + 1]; // approximate solution at node x_i at time level t_k+1
-            }
-            u[k + 1][0] = 0.0; // first boundary condition
-            u[k + 1][nodes_x - 1] =
-                    1.0 - (r / (r + a)) * calerf::ERFCL(a / (2.0 * sqrt(D * (k + 1)))); // second boundary condition
-        }
-        /**
-         * dependence of the maximum absolute value of the error observed for T_MAX as a function of the spatial step h
-         * TODO: save to file results for T_MAX and range of h's
-         **/
-        tk = T_MAX;
-        k = nodes_t - 1;
-        //(double tk, double h, int nodes_x, int k, double *const *u)
-        double max_error = find_max_error(tk, h, nodes_x, k, u);
-        std::cout << "max error: " << max_error << std::endl;
+                res_u = u[k + 1][i] = lambda_dm * u[k][i - 1] + (1.0 - 2.0*lambda_dm) * u[k][i] + lambda_dm * u[k][i + 1];
+                res_a = analytical_solution(tk+dt, xi);
 
-        delete[] ic_array;
+//                if (k == 0) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} //results for T_0
+//                if (k == ((nodes_t - 1)/2)) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} // results in the middle of the time interval
+//                if (k == nodes_t - 2) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} //results for T_MAX
+//                if (k == 12500) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} //results for T=0.5
+                if (k == 37500) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} //results for T=1.5
+
+                xi += h;
+            }
+            u[k + 1][nodes_x - 1] = second_boundary_condition(tk, dt);
+            tk += dt;
+        }
+
+        /**
+         * results saved to file for plotting dependence of the maximum absolute value of the error observed for T_MAX as a function of the spatial step h
+         **/
+        FTCS_h_errors << log10(h) << " " << log10(find_max_error(tk, h, nodes_x, k, u)) << "\n";
+
         free_matrix(u, nodes_t);
     }
 
-    result_file.close();
+    FTCS_h_errors.close();
+    FTCS_results.close();
 }
 
 double find_max_error(double tk, double h, int nodes_x, int k, double *const *u)
@@ -111,12 +120,16 @@ double find_max_error(double tk, double h, int nodes_x, int k, double *const *u)
     double max_error = 0.0;
     double error;
     double xi = X_MIN + h;
+    double res_u, res_a;
 
-    for (int i=1; i<nodes_x-2; i++)
+    for(int i=1; i<nodes_x-1; i++)
     {
-        error = fabs(u[k][i]- analytical_solution(tk, xi));
+//        error = fabs(u[k][i]- analytical_solution(tk, xi));
+        res_u = u[k][i];
+        res_a = analytical_solution(tk, xi);
+        error = fabs(res_u - res_a);
 //        std::cout << "err: i=" << i << " : "<< error << " \n";
-        if (error > max_error)
+        if(error > max_error)
         {
             max_error = error;
 //            std::cout << "max:" << max_error << " \n";
@@ -129,23 +142,27 @@ double find_max_error(double tk, double h, int nodes_x, int k, double *const *u)
 
 double analytical_solution(double tk, double xi)
 {
-    return 1.0 - (r/xi)*double(calerf::ERFCL((xi-r)/(2.0*sqrt(D*tk))));
+    return 1.0 - (r/xi)*static_cast<double>(calerf::ERFCL((xi-r)/(2.0*sqrt(D*tk))));
 }
+
+double first_boundary_condition() { return 0.0; }
+
+double second_boundary_condition(double tk, double dt) { return 1.0 - (r / (r + a)) * static_cast<double>(calerf::ERFCL(a / (2.0 * sqrt(D * (tk + dt))))); }
 
 void fill_array(double *array, int size, double value)
 {
-    for (int i=0; i<size; i++)
+    for(int i=0; i<size; i++)
     {
         array[i] = value;
     }
 }
 
-void print_array(double *array, int size)
+void print_array(double *array, int size, int print_width)
 {
     std::cout << "[ ";
     for(int i=0; i<size; i++)
     {
-        std::cout << array[i] << " ";
+        std::cout << array[i]  << std::setw(print_width);
     }
     std::cout << "] \n";
 }
@@ -153,29 +170,29 @@ void print_array(double *array, int size)
 double **allocate_matrix(int rows, int cols)
 {
     auto** matrix = new double*[rows];
-    for (int i=0; i<rows; ++i)
+    for(int i=0; i<rows; ++i)
         matrix[i] = new double[cols];
     return matrix;
 }
 
 void fill_matrix(double **matrix, int rows, int cols, double value)
 {
-    for (int i=0; i<rows; i++)
+    for(int i=0; i<rows; i++)
     {
-        for (int j=0; j<cols; j++)
+        for(int j=0; j<cols; j++)
         {
             matrix[i][j] = value;
         }
     }
 }
 
-void print_matrix(double *const *matrix, const int rows, const int cols)
+void print_matrix(double *const *matrix, const int rows, const int cols, int print_width)
 {
-    for (int i=rows-1; i>=0; i--)
+    for(int i=rows-1; i>=0; i--)
     {
-        for (int j=0; j<cols; j++)
+        for(int j=0; j<cols; j++)
         {
-            std::cout << matrix[i][j] << std::setw(8);
+            std::cout << matrix[i][j] << std::setw(print_width);
         }
         std::cout  << "\n";
     }
@@ -184,7 +201,7 @@ void print_matrix(double *const *matrix, const int rows, const int cols)
 
 void free_matrix(double *const *matrix, int rows)
 {
-    for (int i=0; i<rows; ++i)
+    for(int i=0; i<rows; ++i)
     {
         delete [] matrix[i];
     }
