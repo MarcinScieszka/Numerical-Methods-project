29406596976ea53460c11421228f4a9705f2ca0d
Marcin Scieszka <marcin.scieszka@student.pk.edu.pl>
2021-06-12 19:14:28 +0200

    Changed FTCS formula to adapt to given equation, improvements

--- main.cpp
+++ main.cpp
@@ -8,48 +8,46 @@
  *  Application of differential methods to an approximate solution of the diffusion equation with initial condition and boundary conditions.
  *
  *  Algorithms used:
- *  - FTCS Method
- *  - Laasonen method using LU decomposition
- *  - Laasonen method using Thomas algorithm
+ *  - FTCS Explicit Method
+ *  - Laasonen method (Backward Time, Centered Space - BTCS Scheme) using LU decomposition
+ *  - Laasonen method using Thomas algorithm (TDMA)"
  *
  *  Initial condition: U(x,0) = 1
- *  First boundary condition: U(r,t) = 0
- *  Second boundary condition: U(r+a, t) = 1.0 - (r/(r+a))*erfc(a/(2.0*sqrt(D*t)))
+ *  First boundary condition: U(rad,t) = 0
+ *  Second boundary condition: U(rad+a, t) = 1.0 - (rad/(rad+a))*erfc(a/(2.0*sqrt(D*t)))
  * */
 
 double const D = 1.0; // diffusion coefficient
 double const T_0 = 0.0; // starting moment
 double const T_MAX = 2.0; // max time
-double const r = 1.0; // radius of the sphere
+double const rad = 1.0; // radius of the sphere
 double const a = 10.0; // domain length
-double const X_MIN = r; // x - spatial coordinate
-double const X_MAX = r + a;
+double const X_MIN = rad; // x - spatial coordinate
+double const X_MAX = rad + a;
 double const lambda_dm = 0.4; // lambda value used for direct methods, NOTE: beware of numerical stability limitations - method stable only when lambda < 0.5
 double const lambda_im = 1.0; // lambda value used for for indirect methods, NOTE: lambda = D*(dt/h*h)
 
-void ftcs_method(); // Forward Time Centered Space Method
+void ftcs_method(); // Forward Time Centered Space Scheme
 void lm_ta(); // Laasonen method using Thomas algorithm
 double analytical_solution(double tk, double xi);
 double get_initial_condition();
 double get_second_boundary_condition(double tk, double dt);
 double get_first_boundary_condition();
 double find_max_error(double tk, double h, int nodes_x, int k, double *const *u);
-double get_alpha();
-double get_beta();
 double get_gamma();
-double get_phi();
-double get_psi();
 double get_theta(double tk, double dt);
 double **allocate_matrix(int rows, int cols);
 void free_matrix(double *const *matrix, int rows);
-void print_array(double *array, int size, int print_width=6);
+void print_array(double *array, int size, int print_width=10);
 void fill_array(double *array, int size, double value);
 void fill_matrix(double **matrix, int rows, int cols, double value);
 
+void determine_eta(double *eta, const double *u, const double *d, const double *l, int nodes_x);
+
 int main()
 {
-//    ftcs_method();
-    lm_ta();
+    ftcs_method();
+//    lm_ta();
 
     return 0;
 }
@@ -79,20 +77,11 @@ void lm_ta()
 
         fill_array(y, nodes_x, get_initial_condition());
 
-
-        /**
-         * obtaining values from given functions from boundary conditions
-         * */
-        alpha = get_alpha();
-        beta = get_beta();
         gamma = get_gamma();
-        phi = get_phi();
-        psi = get_psi();
         theta = get_theta(tk, dt);
 
-        d[0] = -alpha/h + beta;
-        u[0] = alpha/h;
-        l[0] = 0.0;
+        d[0] = 1.0;
+        u[0] = 0.0;
         b[0] = -gamma;
 
         for(i=1; i<nodes_x-1; i++)
@@ -103,15 +92,25 @@ void lm_ta()
             b[i] = -y[i];
         }
 
-        u[nodes_x-2] = 0.0;
-        d[nodes_x-1] = phi/h + psi;
-        l[nodes_x-2] = -phi/h;
+        d[nodes_x-1] = 1.0;
+        l[nodes_x-2] = 0.0;
         b[nodes_x-1] = -theta;
 
+        determine_eta(eta, u, d, l, nodes_x); // determining the vector eta
+        print_array(eta, nodes_x);
 
         delete[] u; delete[] d; delete[] l; delete[] b; delete[] eta; delete[] y;
     }
 
+}
+
+void determine_eta(double *eta, const double *u, const double *d, const double *l, int nodes_x)
+{
+    eta[0] = d[0];
+    for (int i = 1; i < nodes_x; i++)
+    {
+        eta[i] = d[i] - l[i-1] * (1.0 / eta[i-1]) * u[i-1];
+    }
 }
 
 void ftcs_method()
@@ -139,8 +138,8 @@ void ftcs_method()
     FTCS_h_errors << "h max_err\n";
     FTCS_results << "h approx_res analytical_res\n";
 
-//    for(nodes_x = 31; nodes_x<1501; nodes_x+=10)
-    for(nodes_x = 21; nodes_x<22; nodes_x+=50) // 1001 nodes -> h=0.01
+//    for(nodes_x = 21; nodes_x<2001; nodes_x+=20)
+    for(nodes_x = 1001; nodes_x<1002; nodes_x+=50) // 1001 nodes -> h=0.01
     {
         h = a / (nodes_x - 1);
         nodes_t = static_cast<int>((T_MAX-T_0) / ((lambda_dm * h * h) / D)) + 1;
@@ -152,27 +151,24 @@ void ftcs_method()
         tk = T_0;
         for(k = 0; k < nodes_t - 1; k++)
         {
-            xi = X_MIN;
-            u[k + 1][0] = get_first_boundary_condition();
-            xi += h;
+            xi = X_MIN + h;
             for(i = 1; i < nodes_x - 1; i++) // first and last i omitted - values already determined from the initial condition
             {
-//                res_u = u[k + 1][i] = lambda_dm * u[k][i - 1] + (1.0 - 2.0*lambda_dm) * u[k][i] + lambda_dm * u[k][i + 1];
-                u[k + 1][i] = u[k][i] + lambda_dm * (u[k][i - 1] - 2.0*u[k][i] + u[k][i + 1]);
-                res_u = u[k + 1][i];
+                res_u = u[k + 1][i] = lambda_dm * u[k][i - 1]*(1.0 - h/xi) + (1.0 - 2.0*lambda_dm) * u[k][i] + lambda_dm * u[k][i + 1]*(1.0 + h/xi);
                 res_a = analytical_solution(tk + dt, xi);
 
                 /**
                  * data to plot numerical and analytical solutions for a few selected values of time t from the whole interval t - plotted using 1001 x nodes
                  * */
 //                if (k == 0) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} //results for T_0
-                if (k == ((nodes_t - 1)/2)) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} // results in the middle of the time interval
+//                if (k == ((nodes_t - 1)/2)) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} // results in the middle of the time interval
 //                if (k == nodes_t - 2) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} //results for T_MAX
 //                if (k == 12500) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} //results for T=0.5
 //                if (k == 37500) {FTCS_results << xi << " " << res_u << " " << res_a << "\n";} //results for T=1.5
 
                 xi += h;
             }
+            u[k + 1][0] = get_first_boundary_condition();
             u[k + 1][nodes_x - 1] = get_second_boundary_condition(tk, dt);
             tk += dt;
 
@@ -185,7 +181,7 @@ void ftcs_method()
         /**
          * results saved to file for plotting dependence of the maximum absolute value of the error observed for T_MAX as a function of the spatial step h
          **/
-        FTCS_h_errors << log10(h) << " " << log10(find_max_error(tk, h, nodes_x, k, u)) << "\n";
+//        FTCS_h_errors << log10(h) << " " << log10(find_max_error(tk, h, nodes_x, k, u)) << "\n";
 
         free_matrix(u, nodes_t);
     }
@@ -195,17 +191,9 @@ void ftcs_method()
     FTCS_results.close();
 }
 
-double get_theta(double tk, double dt) { return 1.0 - (r / (r + a)) * static_cast<double>(calerf::ERFCL(a / (2.0 * sqrt(D * (tk + dt))))); }
-
-double get_psi() { return 0.0; }
-
-double get_phi() { return 0.0; }
+double get_theta(double tk, double dt) { return 1.0 - (rad / (rad + a)) * static_cast<double>(calerf::ERFCL(a / (2.0 * sqrt(D * (tk + dt))))); }
 
 double get_gamma() { return 0.0; }
-
-double get_beta() { return 0.0; }
-
-double get_alpha() { return 0.0; }
 
 double find_max_error(double tk, double h, int nodes_x, int k, double *const *u)
 {
@@ -218,7 +206,7 @@ double find_max_error(double tk, double h, int nodes_x, int k, double *const *u)
     {
 //        error = fabs(u[k][i]- analytical_solution(tk, xi));
         res_u = u[k][i];
-        res_a = analytical_solution(tk, xi);
+        res_a = analytical_solution(tk, xi); // tk == T_MAX
         error = fabs(res_u - res_a);
 //        std::cout << "err: i=" << i << " : "<< error << " \n";
         if(error > max_error)
@@ -232,13 +220,13 @@ double find_max_error(double tk, double h, int nodes_x, int k, double *const *u)
     return max_error;
 }
 
-double analytical_solution(double tk, double xi) { return 1.0 - (r / xi) * static_cast<double>(calerf::ERFCL((xi - r) / (2.0 * sqrt(D * tk)))); }
+double analytical_solution(double tk, double xi) { return 1.0 - (rad / xi) * static_cast<double>(calerf::ERFCL((xi - rad) / (2.0 * sqrt(D * tk)))); }
 
 double get_initial_condition() { return 1.0; }
 
 double get_first_boundary_condition() { return 0.0; }
 
-double get_second_boundary_condition(double tk, double dt) { return 1.0 - (r / (r + a)) * static_cast<double>(calerf::ERFCL(a / (2.0 * sqrt(D * (tk + dt))))); }
+double get_second_boundary_condition(double tk, double dt) { return 1.0 - (rad / (rad + a)) * static_cast<double>(calerf::ERFCL(a / (2.0 * sqrt(D * (tk + dt))))); }
 
 void fill_array(double *array, int size, double value)
 {
